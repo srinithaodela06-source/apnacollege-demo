c0489805f2ed72d8ec1cdc3070948f19545b2814
srinithaodela06-source <srinithaodela06@gmail.com>
2026-07-21 15:28:31 +0530

    Add files via upload

---
+++ two arrays.py
@@ -0,0 +1,8 @@
+from array import array
+arr1 = array('i' , map(int , input  ("Enter element of first array:  ")split()))
+arr2 = array('i' , map(int , input ("Enter element of  second array;  ")split()))
+common = array('i' )
+for i is arr1 ;
+    if   i  is  arr2  and  i  not  is
+                  common . append(i )
+        print("common elements : " , list (common))
